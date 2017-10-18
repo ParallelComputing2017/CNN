5c1485abdb067e84936ae2a03455530e9a6c7af4
Juan Sebastian Navarro <jsnavarroa@unal.edu.co>
2017-10-17 22:40:27 -0500

    Naive activate cude device function

--- src/CNN/fc_layer_cuda.cu
+++ src/CNN/fc_layer_cuda.cu
@@ -1,4 +1,3 @@
-#pragma once
 #include <stdio.h>
 #include <math.h>
 // For the CUDA runtime routines (prefixed with "cuda_")
@@ -18,8 +17,8 @@
 
 #include "fc_layer_cuda.cuh"
 
-void activate2cuda(tensor_t<float> in, tensor_t<float> out);
-
+void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
+		std::vector<float> input, tensor_t<float> out);
 
 fc_layer_cuda_t::fc_layer_cuda_t(tdsize in_size, int out_size) :
 		in(in_size.x, in_size.y, in_size.z), out(out_size, 1, 1), grads_in(
@@ -36,7 +35,7 @@ fc_layer_cuda_t::fc_layer_cuda_t(tdsize in_size, int out_size) :
 	// 2.19722f = f^-1(0.9) => x where [1 / (1 + exp(-x) ) = 0.9]
 }
 
-float activator_function(float x) {
+__host__ __device__ float activator_function(float x) {
 	//return tanhf( x );
 	float sig = 1.0f / (1.0f + exp(-x));
 	return sig;
@@ -52,7 +51,7 @@ float activator_derivative(float x) {
 void fc_layer_cuda_t::activate(tensor_t<float>& in) {
 	this->in = in;
 	//activate();
-	activate2cuda(in, out);
+	activate2cuda(in, weights, input, out);
 }
 
 int fc_layer_cuda_t::map(point_t d) {
@@ -121,23 +120,59 @@ void fc_layer_cuda_t::updateWeights(tensor_t<float> newWeights) {
  */
 /*****************************************************************************/
 
-__global__ void activate_cuda(tensor_t<float> *d_in, tensor_t<float> *d_out) {
+__device__ float& get(tensor_t<float> *t, int _x, int _y, int _z) {
+	assert(_x >= 0 && _y >= 0 && _z >= 0);
+	assert(_x < t->size.x && _y < t->size.y && _z < t->size.z);
+
+	return t->data[_z * (t->size.x * t->size.y) + _y * (t->size.x) + _x];
+}
+
+__device__ void set(tensor_t<float> *t, int _x, int _y, int _z, float value) {
+	assert(_x >= 0 && _y >= 0 && _z >= 0);
+	assert(_x < t->size.x && _y < t->size.y && _z < t->size.z);
+
+	t->data[_z * (t->size.x * t->size.y) + _y * (t->size.x) + _x] = value;
+}
+
+__global__ void activate_cuda(tensor_t<float> *d_in, tensor_t<float> *d_weights,
+		float *d_input, tensor_t<float> *d_out) {
 
 	int index = (blockDim.x * blockIdx.x) + threadIdx.x;
 
 	printf("Index: %i, in size: (%i, %i, %i) \t", index, d_in->size.x,
 			d_in->size.y, d_in->size.z);
 
+	for (int n = 0; n < d_out->size.x; n++) {
+		float inputv = 0;
+
+		for (int i = 0; i < d_in->size.x; i++)
+			for (int j = 0; j < d_in->size.y; j++)
+				for (int z = 0; z < d_in->size.z; z++) {
+					// map
+					int m = z * (d_in->size.x * d_in->size.y)
+							+ j * (d_in->size.x) + i;
+
+					inputv += get(d_in,i, j, z) * get(d_weights,m, n, 0);
+				}
+
+		*(d_input + n) = inputv;
+
+		get(d_out, n, 0, 0)= activator_function(inputv);
+	}
+
 }
 
 /******************************************************************************
  * Host main routine
  */
-void activate2cuda(tensor_t<float> in, tensor_t<float> out) {
+void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
+		std::vector<float> input, tensor_t<float> out) {
 
 	int blocksPerGrid, threadsPerBlock;
 	int totalThreads;
 	tensor_t<float> *h_in, *d_in;
+	tensor_t<float> *h_weights, *d_weights;
+	float *h_input, *d_input;
 	tensor_t<float> *h_out, *d_out;
 
 	// Get device info
@@ -179,24 +214,25 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> out) {
 		exit(EXIT_FAILURE);
 	}
 
-	// Copy slaves to device
+	// Copy weights
 
-	h_out = &out;
-	int out_mem_size = sizeof(out);
+	h_weights = &weights;
+	int weights_mem_size = sizeof(weights);
 
-	if (h_in == NULL) {
+	if (h_weights == NULL) {
 		fprintf(stderr, "Failed to allocate host vectors!\n");
 		exit(EXIT_FAILURE);
 	}
 
-	err = cudaMalloc((void **) &d_out, out_mem_size);
+	err = cudaMalloc((void **) &d_weights, weights_mem_size);
 	if (err != cudaSuccess) {
 		fprintf(stderr, "Failed to allocate device vector C (error code %s)!\n",
 				cudaGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
 
-	err = cudaMemcpy(d_out, h_out, out_mem_size, cudaMemcpyHostToDevice);
+	err = cudaMemcpy(d_weights, h_weights, weights_mem_size,
+			cudaMemcpyHostToDevice);
 	if (err != cudaSuccess) {
 		fprintf(stderr,
 				"Failed to copy vector C from device to host (error code %s)!\n",
@@ -209,7 +245,11 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> out) {
 	printf("CUDA kernel launch with %d blocks of %d threads. Total: %i\n",
 			blocksPerGrid, threadsPerBlock, totalThreads);
 
-	activate_cuda<<<blocksPerGrid, threadsPerBlock>>>(d_in, d_out);
+	printf("Tensor in: \n");
+	print_tensor(*h_in);
+
+	activate_cuda<<<blocksPerGrid, threadsPerBlock>>>(d_in, d_weights, d_input,
+			d_out);
 
 	err = cudaGetLastError();
 	if (err != cudaSuccess) {
@@ -218,17 +258,41 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> out) {
 		exit(EXIT_FAILURE);
 	}
 
+	// get input array
+
+	h_input = &input[0];
+	int input_mem_size = sizeof(float) * input.size();
+
+	err = cudaMemcpy(h_input, d_input, input_mem_size, cudaMemcpyDeviceToHost);
+	if (err != cudaSuccess) {
+		fprintf(stderr,
+				"Failed to copy vector INPUT from device to host (error code %s)!\n",
+				cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	err = cudaFree(d_input);
+	if (err != cudaSuccess) {
+		fprintf(stderr, "Failed to free device vector INPUT (error code %s)!\n",
+				cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	// Get out
+	h_out = &out;
+	int out_mem_size = sizeof(out);
+
 	err = cudaMemcpy(h_out, d_out, out_mem_size, cudaMemcpyDeviceToHost);
 	if (err != cudaSuccess) {
 		fprintf(stderr,
-				"Failed to copy vector C from device to host (error code %s)!\n",
+				"Failed to copy OUT from device to host (error code %s)!\n",
 				cudaGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
 
 	err = cudaFree(d_out);
 	if (err != cudaSuccess) {
-		fprintf(stderr, "Failed to free device vector C (error code %s)!\n",
+		fprintf(stderr, "Failed to free device OUT (error code %s)!\n",
 				cudaGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
