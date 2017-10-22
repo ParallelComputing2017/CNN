4c40ae267dff432fc3f91b3d8249ddf0d6654885
Juan Sebastian Navarro <jsnavarroa@unal.edu.co>
2017-10-21 19:42:03 -0500

    Get the out tensor from the cuda device

--- src/CNN/fc_layer_cuda.cu
+++ src/CNN/fc_layer_cuda.cu
@@ -126,8 +126,6 @@ __device__ float& get(tensor_t<float> *t, int _x, int _y, int _z) {
 	assert(_x >= 0 && _y >= 0 && _z >= 0);
 	assert(_x < t->size.x && _y < t->size.y && _z < t->size.z);
 
-	printf("get: %i, %i, %i \n", _x, _y, _z);
-
 	return t->data[_z * (t->size.x * t->size.y) + _y * (t->size.x) + _x];
 }
 
@@ -160,12 +158,11 @@ __global__ void activate_cuda(tensor_t<float> *d_in, tensor_t<float> *d_weights,
 					int m = z * (d_in->size.x * d_in->size.y)
 							+ j * (d_in->size.x) + i;
 
-					printf("m : %i \n", m);
-
 					inputv += get(d_in, i, j, z) * get(d_weights, m, n, 0);
 
-					printf("inputv: %f \n", inputv);
 				}
+
+		printf("inputv: %f \n", inputv);
 
 		*(d_input + n) = inputv;
 
@@ -204,7 +201,7 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	int in_mem_size = sizeof(in);
 
 	// TODO remove
-	printf("sizeof(in) , sizeof(out) = %lu * %lu \n", sizeof(in), sizeof(out));
+	printf("out[0,0,0]: %f, \n", out.get(0, 0, 0));
 
 	printf("in: %i, %i, %i \n", in.size.x, in.size.y, in.size.z);
 
@@ -214,11 +211,9 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	}
 
 	cudaMalloc((void **) &d_in, in_mem_size);
-
 	cudaCheckError("cudaMalloc IN tensor");
 
 	cudaMemcpy(d_in, h_in, in_mem_size, cudaMemcpyHostToDevice);
-
 	cudaCheckError("cudaMemcopy to device IN tensor");
 
 	// IN DATA
@@ -229,16 +224,13 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	printf("sizeof(in)= %lu , in_data_size = %lu \n", sizeof(in), in_data_size);
 
 	cudaMalloc((void **) &d_in_data, in_data_size);
-
 	cudaCheckError("cudaMalloc IN tensor data");
 
 	cudaMemcpy(d_in_data, in.data, in_data_size, cudaMemcpyHostToDevice);
-
 	cudaCheckError("cudaMemcpy to device IN tensor data");
 
 	cudaMemcpy(&(d_in->data), &d_in_data, sizeof(d_in->data),
 			cudaMemcpyHostToDevice);
-
 	cudaCheckError("cudaMemcpy Binding pointers of IN tensor data");
 
 	// Copy weights
@@ -255,13 +247,27 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	}
 
 	cudaMalloc((void **) &d_weights, weights_mem_size);
-
 	cudaCheckError("cudaMalloc Weights tensor");
 
-	cudaMemcpy(d_weights, h_weights, weights_mem_size,
-			cudaMemcpyHostToDevice);
-
+	cudaMemcpy(d_weights, h_weights, weights_mem_size, cudaMemcpyHostToDevice);
 	cudaCheckError("cudaMemcpy to device Weights tensor");
+
+	// Weights DATA
+
+	float *d_weights_data;
+	long weights_data_size = sizeof(*d_weights_data) * weights.size.x
+			* weights.size.y * weights.size.z;
+
+	cudaMalloc((void **) &d_weights_data, weights_data_size);
+	cudaCheckError("cudaMalloc Weights tensor data");
+
+	cudaMemcpy(d_weights_data, weights.data, weights_data_size,
+			cudaMemcpyHostToDevice);
+	cudaCheckError("cudaMemcpy to device Weights tensor data");
+
+	cudaMemcpy(&(d_weights->data), &d_weights_data, sizeof(d_weights->data),
+			cudaMemcpyHostToDevice);
+	cudaCheckError("cudaMemcpy Binding pointers of Weights tensor data");
 
 	// Reserve input memory space
 
@@ -271,11 +277,9 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	printf("input_mem_size : %lu \n", input_mem_size);
 
 	cudaMalloc((void **) &d_input, input_mem_size);
-
 	cudaCheckError("cudaMalloc Input array");
 
 	cudaMemcpy(d_input, h_input, input_mem_size, cudaMemcpyHostToDevice);
-
 	cudaCheckError("cudaMemcpy to device Input array");
 
 	// Reserve memory space for OUT
@@ -284,13 +288,30 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	int out_mem_size = sizeof(out);
 
 	cudaMalloc((void **) &d_out, out_mem_size);
-
 	cudaCheckError("cudaMalloc Out tensor");
 
 	cudaMemcpy(d_out, h_out, out_mem_size, cudaMemcpyHostToDevice);
-
 	cudaCheckError("cudaMemcpy to device Out tensor");
 
+	// Out DATA
+
+	float *d_out_data;
+	long out_data_size = sizeof(*d_out_data) * h_out->size.x * h_out->size.y
+			* h_out->size.z;
+
+	printf("out_data_size : %lu \n", out_data_size);
+
+	cudaMalloc((void **) &d_out_data, out_data_size);
+	cudaCheckError("cudaMalloc Out tensor data");
+
+	cudaMemcpy(d_out_data, h_out->data, out_data_size, cudaMemcpyHostToDevice);
+	cudaCheckError("cudaMemcpy to device Out tensor data");
+
+	cudaMemcpy(&(d_out->data), &d_out_data, sizeof(d_out->data),
+			cudaMemcpyHostToDevice);
+	cudaCheckError("cudaMemcpy Binding pointers of Out tensor data");
+
+	printf("h_out[0,0,0]: %f, \n", h_out->get(0, 0, 0));
 	// TODO
 	//printf("Tensor in: \n");
 	//print_tensor(*h_in);
@@ -319,21 +340,17 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	// get input array
 
 	cudaMemcpy(h_input, d_input, input_mem_size, cudaMemcpyDeviceToHost);
-
 	cudaCheckError("cudaMemcpy to host Input array");
 
 	cudaFree(d_input);
-
 	cudaCheckError("cudaFree Input array");
 
-	// Get out
+	// Get Out DATA
 
-	cudaMemcpy(h_out, d_out, out_mem_size, cudaMemcpyDeviceToHost);
-
-	cudaCheckError("cudaMemcpy to host Out tensor");
+	cudaMemcpy(h_out->data, d_out_data, out_data_size, cudaMemcpyDeviceToHost);
+	cudaCheckError("cudaMemcpy to host Out tensor data");
 
 	cudaFree(d_out);
-
 	cudaCheckError("cudaFree Out tensor");
 
 	// Free host memory
@@ -344,6 +361,7 @@ void activate2cuda(tensor_t<float> in, tensor_t<float> weights,
 	// TODO remove
 	printf("cuda out: %i, %i, %i \n", h_out->size.x, h_out->size.y,
 			h_out->size.z);
+	printf("cuda out[0,0,0]: %f, \n", h_out->get(0, 0, 0));
 	print_tensor(*h_out);
 }
 
