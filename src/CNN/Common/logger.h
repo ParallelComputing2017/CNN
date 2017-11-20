55ad6a86ca7b2585dc1e078a0027e0d48e479d79
Juan Sebastian Navarro <jsnavarroa@unal.edu.co>
2017-11-20 18:47:31 -0500

    Print the thread id in the log

--- src/CNN/Common/logger.h
+++ src/CNN/Common/logger.h
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string>
+#include <thread>
+#include <sstream>
 
 using namespace std;
 
@@ -20,23 +22,38 @@ private:
 	static bool const debug_level = false;
 	static bool const info_level = true;
 
+	static string getThreadId() {
+		std::thread::id this_id = std::this_thread::get_id();
+
+		thread::id myid = this_thread::get_id();
+		hash<thread::id> hasher;
+		stringstream ss;
+		ss << hasher(myid);
+		string tId = ss.str();
+		return tId.substr(0, 5);
+	}
+
 public:
 	static void debug(string format, ...) {
+
+		string tId = getThreadId();
 
 		if (debug_level) {
 			va_list args;
 			va_start(args, format);
-			vprintf(("DEBUG  " + format + "\n").c_str(), args);
+			vprintf((tId + "  DEBUG  " + format + "\n").c_str(), args);
 			va_end(args);
 		}
 	}
 
 	static void info(string format, ...) {
 
+		string tId = getThreadId();
+
 		if (info_level) {
 			va_list args;
 			va_start(args, format);
-			vprintf(("INFO  " + format + "\n").c_str(), args);
+			vprintf((tId + "  INFO  " + format + "\n").c_str(), args);
 			va_end(args);
 		}
 	}
